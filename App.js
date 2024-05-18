6271a2e9066f91ad583618b694a62f71cc7ab8c5
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-05-18 14:37:55 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//create ELement will create an element  using basic React Library createElement(tagName, attributes, content)
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "heading1" },
-  "Hello world from React"
-);
-console.log(heading); // it will gives us not an html tag rather than it gives us a react javascript object
+//element
+const Heading = () => <h1 id="head">React element</h1>;
 
-/*
- * for nested element
- */
+//functional component
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ])
-);
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Heading />
+      <h1 id="heading">React Functional Component</h1>
+    </div>
+  );
+};
 
-//REactDOM will create a main root for our code to put the elements into the browser
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//render() it will render our element that we have created
-root.render(parent);
+
+root.render(<HeadingComponent />);
