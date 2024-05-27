2bb96d987290bff18690bbe8aca1cacf4b4bf52a
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-05-27 12:29:17 +0530

    useLayoutEffect Hook for truncated text

---
+++ src/utils/useTruncatedElement.js
@@ -0,0 +1,24 @@
+import React, { useState, useLayoutEffect } from "react";
+
+const useTruncatedElement = ({ ref }) => {
+  const [isTruncated, setIsTruncated] = useState(false);
+  const [isReadingMore, setIsReadingMore] = useState(false);
+
+  useLayoutEffect(() => {
+    const { offsetHeight, scrollHeight } = ref.current || {};
+
+    if (offsetHeight && scrollHeight && offsetHeight < scrollHeight) {
+      setIsTruncated(true);
+    } else {
+      setIsTruncated(false);
+    }
+  }, [ref]);
+
+  return {
+    isTruncated,
+    isReadingMore,
+    setIsReadingMore,
+  };
+};
+
+export default useTruncatedElement;
