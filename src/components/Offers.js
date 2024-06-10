78dbc56c2da4a5719373ed409643ed26fd7e6975
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-10 12:57:24 +0530

    offers page added

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -2,39 +2,10 @@ import React from "react";
 // import UserContext from "../context/UserContext";
 
 class Offers extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-      count1: 0,
-    };
-  }
-
   render() {
     return (
-      <div>
-        <h1>{this.props.name}</h1>
-        <h2>Count : {this.state.count}</h2>
-        <h2>Count 1: {this.state.count1}</h2>
-        <button
-          onClick={() => {
-            this.setState({ count: this.state.count + 1 });
-          }}
-        >
-          Increase Count
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ count1: this.state.count1 + 1 });
-          }}
-        >
-          Increase Count1
-        </button>
-        {/* Context in class Based Components
-        <UserContext.Consumer>
-          {({ loggedInUser }) => <h1>{loggedInUser}</h1>}
-        </UserContext.Consumer> */}
+      <div className="h-screen pt-[120px] max-w-[900px] mx-auto">
+        <h1 className="text-2xl font-bold">Offers Page</h1>
       </div>
     );
   }
