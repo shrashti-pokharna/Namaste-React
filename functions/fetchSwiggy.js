218d0ce0c8798aecfff2c4dd343b3ce7a13afc61
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 15:48:43 +0530

    finalcommit

--- functions/fetchSwiggy.js
+++ functions/fetchSwiggy.js
@@ -1,27 +1,28 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  console.log("in functions");
-  //   const { lat, lng } = event.queryStringParameters;
-  //   console.log("lat and lng", lat, lng);
-  //   if (!lat || !lng) {
-  //     return {
-  //       statusCode: 400,
-  //       body: JSON.stringify({ error: "Missing lat or lng parameter" }),
-  //     };
-  //   }
+  const { lat, lng } = event.queryStringParameters;
+  console.log("lat and lng", lat, lng);
+  if (!lat || !lng) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing lat or lng parameter" }),
+    };
+  }
 
-  const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-  console.log("apiUrl", apiUrl);
+  const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
   try {
-    console.log("in try");
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log("json response", JSON.stringify(data));
+    const response = await fetch(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      },
+    });
 
-    // const response = await axios.get(apiUrl);
-    // console.log("response in fetchswiggy", response);
+    const data = await response.json();
 
     return {
       statusCode: 200,
