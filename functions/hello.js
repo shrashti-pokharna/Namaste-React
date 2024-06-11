d14abb314ce467e704f61aee247d0e185516f554
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 15:21:43 +0530

    commit

--- functions/hello.js
+++ functions/hello.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
   const data = { hello: "Hello World" };
   const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
   const response = await fetch(apiUrl);
-  console.log(response.json);
+  console.log(response);
   const data1 = await response.json();
   console.log(data1);
 
