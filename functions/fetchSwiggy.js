706e28d3d5f39464564f4c6d1624900fb834b3e6
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 10:50:37 +0530

    console added

--- functions/fetchSwiggy.js
+++ functions/fetchSwiggy.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   console.log("in functions");
   const { lat, lng } = event.queryStringParameters;
+  console.log("lat and lng", lat, lng);
   if (!lat || !lng) {
     return {
       statusCode: 400,
@@ -11,6 +12,7 @@ exports.handler = async function (event, context) {
   }
 
   const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+  console.log("apiUrl", apiUrl);
 
   try {
     const response = await axios.get(apiUrl);
@@ -20,6 +22,7 @@ exports.handler = async function (event, context) {
       body: JSON.stringify(response.data),
     };
   } catch (error) {
+    console.log(error);
     return {
       statusCode: error.response ? error.response.status : 500,
       body: JSON.stringify({ error: error.message }),
