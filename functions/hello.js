cc26d0fd3bd81e66a4b8db44899637a2e8ade4c5
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 15:28:14 +0530

    commit

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 exports.handler = async (event, context) => {
   const data = { hello: "Hello World" };
-  const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+  const apiUrl = `https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
   const response = await fetch(apiUrl);
   console.log(response);
   const data1 = await response.json();
