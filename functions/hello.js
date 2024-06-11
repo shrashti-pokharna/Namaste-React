bc99f54e1c4c57b58bc68671989b5a1f1ea1a84d
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 14:42:25 +0530

    commit

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,13 @@
+const fetch = require("node-fetch");
 exports.handler = async (event, context) => {
   const data = { hello: "Hello World" };
   const apiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-  const response = await fetch(apiUrl);
+  const response = await fetch(apiUrl, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
   const data1 = await response.json();
   console.log(data1);
 
