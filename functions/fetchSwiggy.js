2ebc56ff880e09d374edc1e8142fbdb994ffb24c
shrashti-pokharna <shrashti.pokharna@gmail.com>
2024-06-11 14:18:37 +0530

    commit

--- functions/fetchSwiggy.js
+++ functions/fetchSwiggy.js
@@ -1,5 +1,4 @@
-// const axios = require("axios");
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
   console.log("in functions");
